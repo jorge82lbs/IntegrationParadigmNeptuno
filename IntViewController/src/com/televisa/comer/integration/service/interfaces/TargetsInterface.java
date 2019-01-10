ccd9f08a5d3b367bb1bda0fe7780abccde6216ab
jorge82lbs <jorge82lbs@gmail.com>
2019-01-09 21:33:15 -0600

    Se agrega interfaz para la capa de negocio de Targets

---
+++ IntViewController/src/com/televisa/comer/integration/service/interfaces/TargetsInterface.java
@@ -0,0 +1,22 @@
+/**
+* Project: Integraton Paradigm - Landmark
+*
+* File: TargetsInterface.java
+*
+* Created on: Enero 09, 2019 at 11:00
+*
+* Copyright (c) - OMW - 2019
+*/
+package com.televisa.comer.integration.service.interfaces;
+/** Interface que define los metodos a utilizar para el modulo de Targets
+ * en la capa de servicio
+ *
+ * @author Jorge Luis Bautista Santiago - OMW
+ *
+ * @version 01.00.01
+ *
+ * @date Enero 09, 2019, 12:00 pm
+ */
+public interface TargetsInterface {
+    public void auxiliar();
+}
